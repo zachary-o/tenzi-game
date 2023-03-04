3d97d867dc1710c5d9c316c7229402ea5104a95f
zachary-o <104291766+zachary-o@users.noreply.github.com>
2023-03-04 23:15:34 +0200

    upload tenzie game project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+const [number, setNumber] = useState(1)
+
+const diceButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+const getRandomNum = (min, max) => {
+  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+;
+  setNumber(randomNum);
+  console.log(number);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <h1>Tenzies</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Roll until all dices are the same. Click each dice to freeze it at its
+          current value between rolls.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="dice-buttons">
+          {diceButtons.map((diceButton, index) => {
+            return (
+              <button key={index} className="dice-button">
+                {number}
+              </button>
+            );
+          })}
+        </div>
+        <button className="roll-button" onClick={() => getRandomNum(1, 6)}>
+          Roll
+        </button>
+        <button className="change-mode-button">Change Mode</button>
+      </div>
     </div>
   );
 }
